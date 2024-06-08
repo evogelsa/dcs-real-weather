f8ba4eb484d6940f1a80835eda9971c5187f8285
Ethan Vogelsang <ethanvogelsang@gmail.com>
2024-06-07 20:26:26 -0500

    remove unused scanner implementation

--- cmd/bot/handlers/lastmetar.go
+++ cmd/bot/handlers/lastmetar.go
@@ -2,72 +2,14 @@ package handlers
 
 import (
 	"bufio"
-	"bytes"
-	"io"
 	"log"
 	"os"
 	"regexp"
-	"strings"
 
 	dg "github.com/bwmarrin/discordgo"
 
 	"github.com/evogelsa/DCS-real-weather/cmd/bot/config"
 )
-
-type scanner struct {
-	reader io.ReaderAt
-	pos    int64
-	err    error
-	buf    []byte
-}
-
-func (s *scanner) read() {
-	const chnksz = 1024
-
-	if s.pos == 0 {
-		s.err = io.EOF
-		return
-	}
-
-	size := int64(chnksz)
-	if size > s.pos {
-		size = s.pos
-	}
-	s.pos -= size
-	buf := make([]byte, size, size+int64(len(s.buf)))
-
-	_, s.err = s.reader.ReadAt(buf, s.pos)
-	if s.err == nil {
-		s.buf = append(buf, s.buf...)
-	}
-}
-
-func (s *scanner) Line() (string, error) {
-	if s.err != nil {
-		return "", s.err
-	}
-
-	for {
-		lineStart := bytes.LastIndexByte(s.buf, '\n')
-
-		if lineStart >= 0 {
-			var line string
-			line = strings.TrimSpace(string(s.buf[lineStart+1:]))
-			s.buf = s.buf[:lineStart]
-			return line, nil
-		}
-
-		s.read()
-		if s.err != nil {
-			if s.err == io.EOF {
-				if len(s.buf) > 0 {
-					return strings.TrimSpace(string(s.buf)), nil
-				}
-			}
-			return "", s.err
-		}
-	}
-}
 
 var reMETAR = regexp.MustCompile(`^\d{4}/\d{2}/\d{2} \d{2}:\d{2}:\d{2} METAR: (?P<metar>.*)`)
 
